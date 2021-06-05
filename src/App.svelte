dd6d5c6d68f85915090e47bd74d16758591391a4
alv67 <alv67.web@gmail.com>
2021-06-05 16:50:21 +0200

    commit HTML

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,21 @@
-<h1>Test</h1>
+<script>
+  import Timer from './Timer.svelte';
+  import HowTo from './HowTo.svelte';
+</script>
+
+<h1>Lavarsi le mani</h1>
+
+<Timer />
+
+<HowTo />
+
+<h3>
+  <a href="https://www.salute.gov.it/imgs/C_17_opuscoliPoster_193_allegato.pdf">sorgente immagine</a>
+  <a href="https://freesound.org/people/metrostock99/sounds/345086/">sorgente sonora</a>
+</h3>
+
+<style>
+  h1, h3 {
+    text-align: center;
+  }
+</style>
